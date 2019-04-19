80e861c0806aff593aa5fb979e1a7c0b7f05499a
polodev <polodev10@gmail.com>
2019-04-20 01:27:35 +0600

    ______

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,7 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('my-video', require('./components/MyVideo.vue').default);
+Vue.component('video-player', require('./components/VideoPlayer.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
