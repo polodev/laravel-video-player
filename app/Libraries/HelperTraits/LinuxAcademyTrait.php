e9e583eb91850fece6e48feb6ddbd9c30c66e326
polodev <polodev10@gmail.com>
2019-06-03 19:52:58 +0600

    updated linux academy

--- app/Libraries/HelperTraits/LinuxAcademyTrait.php
+++ app/Libraries/HelperTraits/LinuxAcademyTrait.php
@@ -3,9 +3,6 @@
 namespace App\Libraries\HelperTraits;
 
 trait LinuxAcademyTrait {
-
-
-
   public static function linux_academy_linux() {
     $prefix = '/linux-academy/';
     $folders = [
@@ -13,7 +10,7 @@ trait LinuxAcademyTrait {
       [ 'Linux Academy - NGINX Web Server Deep Dive', [] ],
       [ 'Linux Academy Red Hat Certified Systems Administrator Prep Course', [] ],
       [ 'Linux Academy - Using the EC2 Container Service', [] ],
-      [ 'Linux Academy Nginx And The LEMP Stack', [] ],
+      [ 'Linux Academy - Mastering The Linux Command Line', [], ],
     ];
     $folders = self::set_main_id($folders, [12, 47]);
     return self::generate_single_series($prefix, $folders);
--- app/Libraries/HelperTraits/OthersTrait.php
+++ app/Libraries/HelperTraits/OthersTrait.php
@@ -12,7 +12,6 @@ trait OthersTrait {
       [ 'advancedreact-fullstack-advanced-react-graphql', [40, 10,], ],
       [ 'frontendmasters-rapid-development-on-aws-react,-node.js-graphql', [8, 40, 10,], ],
       [ 'hands-on-full-stack-web-development-with-graphql-and-react-[fco]', [40, 10,], ],
-      [ 'Linux Academy - Mastering The Linux Command Line', [12], ],
       [ 'vueschool-vue.js-firebase-realtime-database', [14,], ],
       [ '[Wes Bos] - Learn Node (2017)', [8,], ],
 
