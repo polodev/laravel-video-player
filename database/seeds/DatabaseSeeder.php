75c48ddc1437550fd4e2e23e1fa944e3233468d5
polo dev <polodev10@gmail.com>
2020-08-08 02:03:25 +0600

    updated db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
-        $this->call(SeriesTableSeeder::class);
+        // $this->call(SeriesTableSeeder::class);
     }
 }
