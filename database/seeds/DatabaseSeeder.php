712cfca268e9a193dda92855a08e7db81666517c
polo dev <polodev10@gmail.com>
2020-08-08 12:12:50 +0600

    added manual seeder for series and topics

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(TopicsTableSeeder::class);
+        // $this->call(TopicsTableSeeder::class);
         // $this->call(SeriesTableSeeder::class);
+        $this->call( ManualSeriesAndTopicsSeeder::class );
     }
 }
